4ced3a9b180eead02ed97b113479ab3dfaf61529
Reni Koci <r.koci17@epoka.edu.al>
2019-03-30 11:05:28 +0100

    Subscription update

--- lib/subscriptions.dart
+++ lib/subscriptions.dart
@@ -1,10 +1,43 @@
+import 'package:epokaclubs/subscriptionSample.dart';
 import 'package:flutter/material.dart';
+
 
 class Subscriptions extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-        body: Center(child:  Text('Subscription information'))
+  Widget build(BuildContext ctxt) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new ListDisplay(),
     );
   }
 }
+
+class ListDisplay extends StatelessWidget {
+  @override
+
+  static subscriptionSample item1 = new subscriptionSample("Albert",1);
+  static subscriptionSample item2 = new subscriptionSample("Besjon", 2);
+  static subscriptionSample item3 = new subscriptionSample("Rinald", 3);
+
+  List<subscriptionSample>  subs  = [item1, item2,item3];
+
+  Widget build (BuildContext ctxt) {
+
+    return new Scaffold(
+
+        appBar: new AppBar(title: new Text("Dynamic Demo"),),
+        body: ListView.builder(
+          itemCount: subs.length,
+          itemBuilder:(context, index){
+            return ListTile(
+              leading: Text('${subs[index].id}'),
+              title: Text('${subs[index].name}'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+            );
+          },
+
+    )
+    );
+  }
+}
+
