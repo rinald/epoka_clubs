dba45439e4326992b178b93c8db97f0f71bd6f84
Albert Kopaci <akopaci17@epoka.edu.al>
2019-04-06 12:11:59 +0200

    First club outline. (06.04.2019)

--- lib/events.dart
+++ lib/events.dart
@@ -1,12 +1,42 @@
+import 'package:epokaclubs/models/Event.dart';
+import 'package:epokaclubs/subscriptionSample.dart';
 import 'package:flutter/material.dart';
 
-class Events extends StatelessWidget {
+
+class EventWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-     appBar: AppBar( title: Text('Events')),
-        body: Center(
+  Widget build(BuildContext covariant) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new ListEvents(),
+    );
+  }
+}
+
+class ListEvents extends StatelessWidget {
+  @override
+
+
+  static Event newEvent = new Event("Programing Club","213214","Kristjan",3);
+
+  List<Event>  _event  = [newEvent];
+
+  Widget build (BuildContext covariant) {
+
+    return new Scaffold(
+
+        appBar: new AppBar(title: new Text("Events Page"),),
+        body: ListView.builder(
+          itemCount: _event.length,
+          itemBuilder:(context, index){
+            return ListTile(
+              title: Text('${_event[index].Title}'),
+              leading: Text('${_event[index].club.admin}'),
+            );
+          },
+
         )
     );
   }
 }
+
