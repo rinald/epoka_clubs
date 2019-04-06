04640d777780fa56febbc3405bfc243c8e587b27
Reni Koci <r.koci17@epoka.edu.al>
2019-04-06 12:12:06 +0200

    By Halit(WHY IM NOT AUTHOR)

--- lib/subscriptions.dart
+++ lib/subscriptions.dart
@@ -18,21 +18,23 @@ class ListDisplay extends StatelessWidget {
   static subscriptionSample item1 = new subscriptionSample("Albert",1);
   static subscriptionSample item2 = new subscriptionSample("Besjon", 2);
   static subscriptionSample item3 = new subscriptionSample("Rinald", 3);
+  static subscriptionSample item4 = new subscriptionSample("Halit",4);
+  static subscriptionSample item5 = new subscriptionSample("Kristijan",5);
 
-  List<subscriptionSample>  subs  = [item1, item2,item3];
+  List<subscriptionSample>  subs  = [item1, item2,item3,item4,item5];
 
   Widget build (BuildContext ctxt) {
 
     return new Scaffold(
 
-        appBar: new AppBar(title: new Text("Dynamic Demo"),),
+        appBar: new AppBar(title: new Text("Subsribers"),),
         body: ListView.builder(
           itemCount: subs.length,
           itemBuilder:(context, index){
             return ListTile(
               leading: Text('${subs[index].id}'),
               title: Text('${subs[index].name}'),
-              trailing: Icon(Icons.keyboard_arrow_right),
+              trailing: Icon(Icons.message),
             );
           },
 
